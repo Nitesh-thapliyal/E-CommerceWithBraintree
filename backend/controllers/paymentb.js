e4beccb0c9f8e2166eeebef7738544fedca10385
Nitesh-thapliyal <niteshnits.thapliyal@gmail.com>
2023-01-27 13:52:28 +0530

    add braintree gateway

--- backend/controllers/paymentb.js
+++ backend/controllers/paymentb.js
@@ -1,40 +1,39 @@
 const braintree = require("braintree");
 
 const gateway = new braintree.BraintreeGateway({
-  environment: braintree.Environment.Sandbox,
-  merchantId: "useYourMerchantId",
-  publicKey: "useYourPublicKey",
-  privateKey: "useYourPrivateKey"
+	environment: braintree.Environment.Sandbox,
+	merchantId: process.env.BMERCHANT_ID,
+	publicKey: process.env.BPUBLIC_KEY,
+	privateKey: process.env.BPRIVATE_KEY
 });
 
 exports.getToken = (req, res) => {
-    gateway.clientToken.generate({}, function (err, response)  {
-        if(err){
-            res.status(500).json(err)
-        }
-        else{
-            res.send(response)
-        }
-      });
-}
+	gateway.clientToken.generate({}, (err, response) => {
+		if (err) {
+			res.status(500).json(err);
+		} else {
+			res.send(response);
+		}
+	});
+};
 
-exports.processPayment = () => {
-
-    let nonceFromTheClient = req.body.paymentMethodNonce
-
-    let amountFromTheClient = req.body.amount
-    gateway.transaction.sale({
-        amount: amountFromTheClient,
-        paymentMethodNonce: nonceFromTheClient,
-        options: {
-          submitForSettlement: true
-        }
-      }, function (err, result) {
-            if(err){
-                res.status(500).json(err)
-            }
-            else{
-                res.json(result)
-            }
-      });    
-}
+exports.processPayment = (req, res) => {
+	let nonceFromTheClient = req.body.paymentMethodNonce;
+	let amountFromTheClient = req.body.amount;
+	gateway.transaction.sale(
+		{
+			amount: amountFromTheClient,
+			paymentMethodNonce: nonceFromTheClient,
+			options: {
+				submitForSettlement: true
+			}
+		},
+		(err, result) => {
+			if (err) {
+				res.status(500).json(err);
+			} else {
+				res.status(200).json(result);
+			}
+		}
+	);
+};
--- frontend/src/core/Cart.js
+++ frontend/src/core/Cart.js
@@ -3,6 +3,7 @@ import "../styles.css";
 import Base from "./Base";
 import Card from "./Card";
 import { loadCart } from "./helper/cartHelper";
+import Paymentb from "./Paymentb";
 
 
 const Cart = () => {
@@ -13,7 +14,7 @@ const Cart = () => {
     setProducts(loadCart());
   }, [reload]);
 
-  const loadAllProducts = () => {
+  const loadAllProducts = products => {
     return (
       <div>
         <h2>This section is to load products</h2>
@@ -41,8 +42,8 @@ const Cart = () => {
   return (
     <Base title="Cart Page" description="Ready to checkout">
       <div className="row text-center">
-        <div className="col-6">{loadAllProducts()}</div>
-        <div className="col-6">{loadCheckout()}</div>
+        <div className="col-6">{products.length > 0 ? loadAllProducts(products): (<h3>No Products in Cart</h3>)}</div>
+        <div className="col-6"><Paymentb products = {products} setReload={setReload}/></div>
       </div>
     </Base>
   );
